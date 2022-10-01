8eebe2470961661301a96381e5b557fe9b5e2cfd
roctim17m <mdrokibulislamroctim@gmail.com>
2022-10-02 00:51:28 +0600

    done

--- App.js
+++ App.js
@@ -1,27 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { ImageBackground, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import CarsList from './Components/CarsList';
 import Header from './Components/Header/Index';
-import StyledButton from './Components/StyledButton';
+// import CarsList from './components/CarsList';
+// import Header from './components/Header';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      
       <Header></Header>
-<View style={styles.carContainer}>
-  <ImageBackground source={require('./assets/images/ModelX.jpeg')} style={styles.image}>
-
-  </ImageBackground>
-  <View style={styles.titles}>
-    <Text style={styles.title}>Model S</Text>
-    <Text style={styles.subtitle}>Starting at $65,542</Text>
-  </View>
-
-<StyledButton></StyledButton>
-</View>
-
-
-
+      <CarsList></CarsList>
+     
       <StatusBar style="auto" />
     </View>
   );
@@ -34,27 +24,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  carContainer:{
-width:'100%',
-height:'100%',
-  },
-  titles:{
-marginTop:'30%',
-width:"100%",
-alignItems:'center'
-  },
-  title:{
-    fontSize: 40,
-    fontWeight:'500',
-  },
-  subtitle:{
-fontSize:16,
-color:'#5c5e62'
-  },
-  image:{
-width:'100%',
-height:'100%',
-resizeMode:'cover',
-position:'absolute',
-  }
 });
